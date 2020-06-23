4a1b259ca343d7e3bc98bf35cb1a19d85c03e3a2
clément <clement.abs@gmail.com>
2020-06-23 16:14:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var i;
 /**
  * Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
  */
-app.use('/', express.static(http://chatgbconcept.herokuapp.com + '/public'));
+app.use('/index.html', express.static(__dirname + '/public'));
 
 /**
  * Liste des utilisateurs connectés
